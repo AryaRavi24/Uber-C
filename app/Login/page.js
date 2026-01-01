36329b7f5b63b4655b5dfeb20435f30850ab28a5
AryaRavi24 <aryaravi2022@gmail.com>
2026-01-01 23:31:43 +0530

    Added few changes in login and sign up

--- app/Login/page.js
+++ app/Login/page.js
@@ -1,16 +1,29 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 function page() {
   return (
-    <div className=' w-full h-screen flex justify-center items-center'>
-        <div className='border w-[35%] p-18 rounded-lg'>
-            <h1 className='text-2xl font-semibold'>Login</h1>
-            <input type='text' placeholder='Enter Phone Number or Email' className='border border-neutral-400 w-full p-2 rounded-lg mt-5'/>
-            <input type='password' placeholder='Enter Email' className='border border-neutral-400 w-full p-2 rounded-lg mt-5'/>
-            <button className='mt-5 text-center border w-full p-3 bg-black text-white font-semibold rounded-lg'>Login</button>
-            <p className='mt-5 text-center'>Don't have an account?<Link href='/Register' className='px-1 text-blue-700'>Sign Up</Link></p>
+    <div className=' w-full flex'>
+      <div className='w-1/2 h-screen bg-black flex justify-center items-center'>
+          <div className='w-1/2 h-1/2 border '>
+            <Image src='/uber-logo.png' width={150} height={150} alt='uber logo' className='filter-white py-8' />
+            <h1 className='text-white text-4xl font-semibold'>Move Smarter</h1>
+            <h1 className='text-white text-4xl mt-6 font-semibold'>Travel Faster</h1>
+
+          </div>
         </div>
+        
+        <div className='border w-1/2 flex justify-center items-center'>
+          <div className='p-18 rounded-lg w-3/4 '>
+            <h1 className='text-2xl font-semibold'>Login</h1>
+             <input type='text' placeholder='Enter Phone Number or Email' className='bg-neutral-100 w-full p-3 rounded-lg mt-5 focus:outline-black'/>
+             <input type='password' placeholder='Enter Email' className=' bg-neutral-100 w-full p-3 rounded-lg mt-5 focus:outline-black'/>
+             <button className='mt-5 text-center border w-full p-3 bg-black text-white font-semibold rounded-lg'>Login</button>
+             <p className='mt-5 text-center'>Don't have an account?<Link href='/Register' className='px-1 text-blue-700'>Sign Up</Link></p>
+          </div>
+        </div>
+        
     </div>
   )
 }
--- app/Register/page.js
+++ app/Register/page.js
@@ -1,18 +1,33 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 function page() {
   return (
-    <div className=' w-full h-screen flex justify-center items-center'>
-        <div className='border w-[35%] p-18 rounded-lg'>
+    <>
+    
+    <div className=' w-full flex'>
+        
+        <div className='border w-1/2 flex justify-center items-center'>
+          <div className='p-18 rounded-lg w-3/4 '>
             <h1 className='text-2xl font-semibold'>Sign Up</h1>
-            <input type='text' placeholder='Enter Name' className='border border-neutral-400 w-full p-2 rounded-lg mt-5'/>
-            <input type='text' placeholder='Enter Phone Number' className='border border-neutral-400 w-full p-2 rounded-lg mt-5'/>
-            <input type='password' placeholder='Enter Email' className='border border-neutral-400 w-full p-2 rounded-lg mt-5'/>
-            <button className='mt-5 text-center border w-full p-3 bg-black text-white font-semibold rounded-lg'>Login</button>
+            <input type='text' placeholder='Enter Name' className=' bg-neutral-100 w-full p-3 rounded-lg mt-5  focus:outline-black'/>
+            <input type='text' placeholder='Enter Phone Number' className='bg-neutral-100 w-full p-3 rounded-lg mt-5 focus:outline-black'/>
+            <input type='password' placeholder='Enter Email' className='bg-neutral-100 w-full p-3 rounded-lg mt-5 focus:outline-black'/>
+            <button className='mt-5 text-center border w-full p-3 bg-black text-white font-semibold rounded-lg'>Submit</button>
             <p className='mt-5 text-center'>Already have account?<Link href='/Login' className='px-1 text-blue-700'>Login</Link></p>
+          </div>
+        </div>
+        <div className='w-1/2 h-screen bg-black flex justify-center items-center'>
+          <div className='w-1/2 h-1/2 border '>
+            <Image src='/uber-logo.png' width={150} height={150} alt='uber logo' className='filter-white py-8' />
+            <h1 className='text-white text-4xl font-semibold'>Go anywhere.</h1>
+            <h1 className='text-white text-4xl mt-6 font-semibold'>Effortlessly</h1>
+
+          </div>
         </div>
     </div>
+    </>
   )
 }
 
